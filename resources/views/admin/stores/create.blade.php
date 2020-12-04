e4d5012a133f8b5ea9deb6f3669d76e7591d57a3
eric-araujo <eric.araujolima@hotmail.com>
2020-12-03 21:08:41 -0300

    Utilizando template blade

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -1,35 +1,39 @@
-<h1>Criar Loja</h1>
-<form action="/admin/stores/store" method="POST">
-    @csrf
-<div>
-    <label>Nome da Loja</label>
-    <input type="text" name="name">
-</div>
-<div>
-    <label>Descrição</label>
-    <input type="text" name="description">
-</div>
-<div>
-    <label>Telefone</label>
-    <input type="text" name="phone">
-</div>
-<div>
-    <label>Celular/Whatsapp</label>
-    <input type="text" name="mobile_phone">
-</div>
-<div>
-    <label>Slug</label>
-    <input type="text" name="slug">
-</div>
-<div>
-    <label>Usuário</label>
-    <select name="user">
-        @foreach ($users as $user)
-            <option value="{{$user->id}}">{{$user->name}}</option>    
-        @endforeach
-    </select>
-</div>
-<div>
-    <button type="submit">Criar Loja</button>
-</div>
-</form>
+@extends('layouts.app')
+
+@section('content')
+    <h1>Criar Loja</h1>
+    <form action="/admin/stores/store" method="POST">
+        @csrf
+        <div class="form-group">
+            <label>Nome da Loja</label>
+            <input type="text" class="form-control" name="name">
+        </div>
+        <div class="form-group">
+            <label>Descrição</label>
+            <input type="text" class="form-control" name="description">
+        </div>
+        <div class="form-group">
+            <label>Telefone</label>
+            <input type="text" class="form-control" name="phone">
+        </div>
+        <div class="form-group">
+            <label>Celular/Whatsapp</label>
+            <input type="text" class="form-control" name="mobile_phone">
+        </div>
+        <div class="form-group">
+            <label>Slug</label>
+            <input type="text" class="form-control" name="slug">
+        </div>
+        <div class="form-group">
+            <label>Usuário</label>
+            <select class="form-control" name="user">
+                @foreach ($users as $user)
+                    <option value="{{$user->id}}">{{$user->name}}</option>    
+                @endforeach
+            </select>
+        </div>
+        <div>
+            <button class="btn btn-lg btn-success" type="submit">Criar Loja</button>
+        </div>
+    </form>
+@endsection
--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -1,20 +1,23 @@
-<table>
-    <thead>
-    <tr>
-        <th>#</th>
-        <th>Loja</th>
-        <th>Ações</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($stores as $store)
-        <tr>
-            <th>{{$store->id}}</th>
-            <th>{{$store->name}}</th>
-            <th></th>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+@extends('layouts.app')
 
-{{$stores->links()}}
+@section('content')
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Loja</th>
+            <th>Ações</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($stores as $store)
+            <tr>
+                <th>{{$store->id}}</th>
+                <th>{{$store->name}}</th>
+                <th></th>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    {{$stores->links()}}
+@endsection
