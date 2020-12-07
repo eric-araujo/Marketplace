8cbcdb5f2c39efb2bb3bd068307e115be3fb6ccc
eric-araujo <eric.araujolima@hotmail.com>
2020-12-07 16:53:10 -0300

    Adicionado validação para quando não for selecionada nenhuma categoria

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -58,10 +58,12 @@ class ProductController extends Controller
     {
 
         $data = $request->all();
+        $categories = $request->get('categories', null);
 
         $store = auth()->user()->store;
         $product = $store->products()->create($data);
-        $product->categories()->sync($data['categories']);
+
+        $product->categories()->sync($categories);
 
         if($request->hasFile('photos')){
             $images = $this->imageUpload($request->file('photos'), 'image');
@@ -97,10 +99,14 @@ class ProductController extends Controller
     public function update(ProductRequest $request, $product)
     {
         $data = $request->all();
+        $categories = $request->get('categories', null);
 
         $product = $this->product->find($product);
         $product->update($data);
-        $product->categories()->sync($data['categories']);
+
+        if(!is_null($categories)){
+            $product->categories()->sync($categories);
+        }
 
         if($request->hasFile('photos')){
             $images = $this->imageUpload($request->file('photos'), 'image');
