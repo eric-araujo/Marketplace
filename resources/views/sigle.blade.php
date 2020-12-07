624a3648335b6878133119d4117f6932858d94eb
eric-araujo <eric.araujolima@hotmail.com>
2020-12-07 18:23:38 -0300

    Exibindo imagens na tela de produto para o usuário

--- resources/views/sigle.blade.php
+++ resources/views/sigle.blade.php
@@ -2,14 +2,27 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            @if ($product->photos->count())
+                <img src="{{asset('storage/' . $product->photos->first()->image)}}" alt="" class="card-img-top mb-4">
+                <div class="row">
+                    @foreach ($product->photos as $photo)
+                        <div class="col-md-5">
+                            <img src="{{asset('storage/' . $photo->image)}}" alt="" class="img-fluid">
+                        </div>
+                    @endforeach
+                </div>
+            @else
+                <img src="{{asset('assets/img/no-photo.jpg')}}" alt="" class="card-img-top">
+            @endif
+        </div>
         <div class="col-md-8">
             <h2>{{$product->name}}</h2>
             <p>
                 {{$product->description}}
             </p>
             <h3>
-                {{$product->price}}
+                R$ {{number_format($product->price, '2', ',', '.')}}
             </h3>
             <span>
                 Loja: {{$product->store->name}}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     @foreach ($products as $key => $product)
         <div class="col-md-4">
             <div class="card" style="width: 98%; height:100%;">
-                <a href="{{route('product.sigle', ['slug' => $product->slug])}}" class="btn">
+                
                     @if ($product->photos->count())
                         <img src="{{asset('storage/' . $product->photos->first()->image)}}" alt="" class="card-img-top">
                     @else
@@ -14,8 +14,13 @@
                     <div class="card-body">
                         <h2 class="card-title">{{$product->name}}</h2>
                         <p class="card-text">{{$product->description}}</p>
+                        <h3>
+                            R$ {{number_format($product->price, '2', ',', '.')}}
+                        </h3>
+                        <a href="{{route('product.sigle', ['slug' => $product->slug])}}" class="btn btn-success">
+                            Ver Produto
+                        </a>
                     </div>
-                </a>
             </div>
         </div>
         @if (($key + 1) % 3 == 0)
