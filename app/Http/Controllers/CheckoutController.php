7e7a84a67a01aa0e3e16cbc294e66cd50c260806
eric-araujo <eric.araujolima@hotmail.com>
2020-12-09 14:36:25 -0300

    Configurando api pagseguro

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -8,7 +8,7 @@ class CheckoutController extends Controller
 {
     public function index()
     {
-        if(!auth()->check()){
+        if (!auth()->check()) {
             return redirect()->route('login');
         }
 
@@ -19,17 +19,103 @@ class CheckoutController extends Controller
 
     public function proccess(Request $request)
     {
-        dd($request->all());
+        $dataPost = $request->all();
+
+        $reference = 'XPTO';
+        $creditCard = new \PagSeguro\Domains\Requests\DirectPayment\CreditCard();
+
+        $creditCard->setReceiverEmail(env('PAGSEGURO_EMAIL'));
+        $creditCard->setReference($reference);
+        $creditCard->setCurrency("BRL");
+
+        $cartItems = session()->get('cart');
+
+        foreach ($cartItems as $item) {
+            $creditCard->addItems()->withParameters(
+                $reference,
+                $item['name'],
+                $item['amount'],
+                $item['price'],
+            );   
+        }
+
+        $user = auth->user();
+        $email = env('PAGSEGURO_ENV' == 'sandbox' ? 'test@sandbox.pagseguro.comb.br' : $user->email);
+
+        $creditCard->setSender()->setName($user->name);
+        $creditCard->setSender()->setEmail($email);
+        $creditCard->setSender()->setPhone()->withParameters(
+            11,
+            56273440
+        );
+
+        $creditCard->setSender()->setDocument()->withParameters(
+            'CPF',
+            '16292457044'
+        );
+
+        $creditCard->setSender()->setHash($dataPost['hash']);
+
+        $creditCard->setSender()->setIp('127.0.0.0');
+
+        $creditCard->setShipping()->setAddress()->withParameters(
+            'Av. Brig. Faria Lima',
+            '1384',
+            'Jardim Paulistano',
+            '01452002',
+            'São Paulo',
+            'SP',
+            'BRA',
+            'apto. 114'
+        );
+
+        $creditCard->setBilling()->setAddress()->withParameters(
+            'Av. Brig. Faria Lima',
+            '1384',
+            'Jardim Paulistano',
+            '01452002',
+            'São Paulo',
+            'SP',
+            'BRA',
+            'apto. 114'
+        );
+
+        $creditCard->setToken($dataPost['card_token']);
+
+        list($quantity, $installmentAmount) = explode('|', $dataPost['installment']);
+
+        $creditCard->setInstallment()->withParameters($quantity, $installmentAmount);
+
+        $creditCard->setHolder()->setBirthdate('01/10/1979');
+        $creditCard->setHolder()->setName($dataPost['card_name']);
+
+        $creditCard->setHolder()->setPhone()->withParameters(
+            11,
+            56273440
+        );
+
+        $creditCard->setHolder()->setDocument()->withParameters(
+            'CPF',
+            '16292457044'
+        );
+
+        $creditCard->setMode('DEFAULT');
+
+        $result = $creditCard->register(
+            \PagSeguro\Configuration\Configure::getAccountCredentials()
+        );
+
+        var_dump($result);
     }
 
     private function makePagSeguroSession()
     {
-        if(!session()->has('pagseguro_session_code')){
+        if (!session()->has('pagseguro_session_code')) {
             $sessionCode = \PagSeguro\Services\Session::create(
                 \PagSeguro\Configuration\Configure::getAccountCredentials()
             );
 
-            session()->put('pagseguro_session_code',  $sessionCode->getResult());
+            session()->put('pagseguro_session_code', $sessionCode->getResult());
         }
 
         return;
