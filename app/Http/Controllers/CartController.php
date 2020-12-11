659cf9bf1551d2b99bd573e0311abfbe9efe4e13
eric-araujo <eric.araujolima@hotmail.com>
2020-12-11 13:36:31 -0300

    Adicionando validação aos items que são adicionados no carrinho

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Product;
+
 class CartController extends Controller
 {
 
@@ -16,7 +18,17 @@ class CartController extends Controller
 
     public function add(Request $request)
     {
-        $product = $request->get('product');
+        $productData = $request->get('product');
+
+        $product = Product::whereSlug($productData['slug']);
+        
+        if(!$product->count() || $productData['amount'] == 0){
+            flash('Falha ao adicionar produto no carrinho de compras')->warning();
+            return redirect()->route('home');
+        }
+
+        $product = $product->first(['name', 'price'])->toArray();
+        $product = array_merge($productData, $product);
 
         if (session()->has('cart')) {
 
