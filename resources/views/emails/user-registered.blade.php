7cf906ef344d534b40afb0ed7ea9e9ec52a79665
eric-araujo <eric.araujolima@hotmail.com>
2020-12-14 15:46:46 -0300

    Criado classe para notificação

--- resources/views/emails/user-registered.blade.php
+++ resources/views/emails/user-registered.blade.php
@@ -6,7 +6,7 @@
 <h3>Obrigado por sua inscrição! :)</h3>
 
 <p>
-    Faça bom proveito e excelentes compras em nosso marketplace<br>
+    Faça bom proveito e excelentes compras em nosso marketplace.<br>
     Seu email de cadastro é: <strong>{{$user->email}}</strong><br>
     Sua senha: <strong>Por questões de segurança não enviamos sua senha</strong>
 </p>
